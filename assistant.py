bcd7a845a457c52041f32c918e22eeaca78017a8
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-07-18 08:36:40 +0300

    fix of tool calling bug

--- assistant.py
+++ assistant.py
@@ -8,15 +8,15 @@ class Assistant:
     def __call__(self, state, config: RunnableConfig):
         while True:
             configuration = config.get("configurable", {})
-            passenger_id = configuration.get("passenger_id", None)
-            state = {**state, "user_info": passenger_id}
+            thread_id = configuration.get("thread_id", None)
+            state = {**state, "user_info": thread_id}
             result = self.runnable.invoke(state)
             if not result.tool_calls and (
                 not result.content
                 or isinstance(result.content, list)
                 and not result.content[0].get("text")
             ):
-                messages = state["messages"] + [("user", "Respond with a real output.")]
+                messages = state["messages"] + [("user", "Respond but do not mention the tool the user already knows you are an expert ")]
                 state = {**state, "messages": messages}
             else:
                 break
--- tools.py
+++ tools.py
@@ -2,4 +2,4 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 def create_tools():
-    return [TavilySearchResults(max_results=1)]
+    return [TavilySearchResults(max_results=5)]

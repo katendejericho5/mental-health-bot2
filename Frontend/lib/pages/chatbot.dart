14ae81f178c4f5883420e635aa8bcd080ba8826e
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2024-07-20 00:07:08 +0300

    Chatbot second version

--- Frontend/lib/pages/chatbot.dart
+++ Frontend/lib/pages/chatbot.dart
@@ -15,7 +15,7 @@ class _ChatBotState extends State<ChatBot> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 37, 150, 190),
       ),
-      body: Column(
+      body: ListView(
         children: [
           Container(
             color: Color.fromARGB(179, 37, 149, 190),
@@ -29,6 +29,63 @@ class _ChatBotState extends State<ChatBot> {
                   color: Color.fromARGB(84, 152, 216, 240),
                 ),
               ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage('lib/images/ponytail.png'),
+                  height: 100,
+                ),
+                Container(
+                  height: 110,
+                  width: 220,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(33),
+                    color: Color.fromARGB(193, 10, 83, 110),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Center(
+            child: Container(
+              height: 50,
+              width: 240,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(179, 37, 149, 190),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Container(
+                  height: 110,
+                  width: 220,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(33),
+                    color: Color.fromARGB(193, 10, 83, 110),
+                  ),
+                ),
+                Image(
+                  image: AssetImage('lib/images/ponytail.png'),
+                  height: 100,
+                ),
+              ],
             ),
           ),
         ],

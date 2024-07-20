4a02a6e78cea105a58be3026fa3b0690f4c1e08c
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2024-07-20 08:39:49 +0300

    Working cha section, only remaining with fetching replies

--- Frontend/lib/pages/OTHERS/Profile.dart
+++ Frontend/lib/pages/OTHERS/Profile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mentalhealth/pages/OTHERS/Drawer.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key});
@@ -8,74 +7,98 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'USER PROFILE',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Color.fromARGB(148, 12, 50, 70),
+        backgroundColor: Color.fromARGB(255, 37, 150, 190),
       ),
-      drawer: CustomDrawer(),
       backgroundColor: Color.fromARGB(255, 205, 223, 238),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('lib/images/marvin.jpeg'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Rusoke Marvin',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'rusokemarvin@gmail.com',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 20),
-            Card(
-              elevation: 3,
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Additional Information',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Column(
+        children: [
+          Container(
+            color: Color.fromARGB(179, 37, 149, 190),
+            height: 80,
+            child: Center(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'User Profile',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Color.fromARGB(146, 0, 0, 0),
                     ),
-                    SizedBox(height: 10),
-                    ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text('Age: 22'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.person_outline),
-                      title: Text('Gender: Male'),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.location_on),
-                      title: Text('Location: Uganda'),
-                    ),
-                  ],
+                  ),
+                ),
+                height: 50,
+                width: 280,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(84, 152, 216, 240),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 7,
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('lib/images/marvin.jpeg'),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Rusoke Marvin',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'rusokemarvin@gmail.com',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Card(
+                  elevation: 3,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Additional Information',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        ListTile(
+                          leading: Icon(Icons.person),
+                          title: Text('Age: 22'),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.person_outline),
+                          title: Text('Gender: Male'),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.location_on),
+                          title: Text('Location: Uganda'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- Frontend/lib/pages/chatbot.dart
+++ Frontend/lib/pages/chatbot.dart
@@ -8,6 +8,18 @@ class ChatBot extends StatefulWidget {
 }
 
 class _ChatBotState extends State<ChatBot> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _messages = [];
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _messages.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,105 +27,132 @@ class _ChatBotState extends State<ChatBot> {
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 37, 150, 190),
       ),
-      body: ListView(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            color: Color.fromARGB(179, 37, 149, 190),
-            height: 80,
-            child: Center(
-              child: Container(
-                child: Center(
-                    child: Text(
-                  'Companion Mode',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                      color: Color.fromARGB(146, 0, 0, 0)),
-                )),
-                height: 50,
-                width: 280,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color.fromARGB(84, 152, 216, 240),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+          Expanded(
+            child: ListView(
               children: [
-                const Image(
-                  image: AssetImage('lib/images/ponytail.png'),
-                  height: 100,
-                ),
                 Container(
-                  height: 110,
-                  width: 220,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(33),
-                    color: Color.fromARGB(193, 10, 83, 110),
-                  ),
-                  child: const Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Hi! This is Becky your Companion, how can\ni help you',
-                      style: TextStyle(color: Colors.white),
+                  color: Color.fromARGB(179, 37, 149, 190),
+                  height: 80,
+                  child: Center(
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Companion Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            color: Color.fromARGB(146, 0, 0, 0),
+                          ),
+                        ),
+                      ),
+                      height: 50,
+                      width: 280,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(84, 152, 216, 240),
+                      ),
                     ),
-                  )),
-                )
+                  ),
+                ),
+                SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Image(
+                        image: AssetImage('lib/images/ponytail.png'),
+                        height: 100,
+                      ),
+                      Container(
+                        height: 110,
+                        width: 220,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(33),
+                          color: Color.fromARGB(193, 10, 83, 110),
+                        ),
+                        child: const Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              'Hi! This is Becky your Companion, how can\ni help you',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 25),
+                ..._messages.map((message) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 110,
+                          width: 220,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(33),
+                            color: Color.fromARGB(255, 155, 214, 236),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Text(
+                                message,
+                                style: TextStyle(
+                                  color: Color.fromARGB(193, 10, 83, 110),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Image(
+                          image: AssetImage('lib/images/manager.png'),
+                          height: 90,
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
               ],
             ),
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          Center(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Container(
               height: 50,
-              width: 240,
+              width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
                 color: Color.fromARGB(179, 37, 149, 190),
               ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Container(
-                  height: 110,
-                  width: 220,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(33),
-                    color: Color.fromARGB(255, 155, 214, 236),
-                  ),
-                  child: const Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Okay! This is Jessy and i have a few questions\nfor you',
-                      style: TextStyle(
-                        color: Color.fromARGB(193, 10, 83, 110),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Type a message',
+                        hintStyle: TextStyle(color: Colors.white),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
                       ),
                     ),
-                  )),
-                ),
-                Image(
-                  image: AssetImage('lib/images/manager.png'),
-                  height: 90,
-                ),
-              ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.send, color: Colors.white),
+                    onPressed: _sendMessage,
+                  ),
+                ],
+              ),
             ),
           ),
+          SizedBox(height: 15),
         ],
       ),
     );

bca8c4f72f9f5263b78c86ad51911f566ebe4f2f
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-07-20 20:34:08 +0300

    merge conflitct fix

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mentalhealth/pages/HOME/companion.dart';
-import 'package:mentalhealth/pages/HOME/homepage.dart';
-import 'package:mentalhealth/pages/WelcomeScreen.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-// import 'pages/HOME/therapist.dart';
+import 'package:mentalhealth/screens/homepage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const WelcomeScreen(),
-      routes: {
-        // '/home': (context) => const MentalHealthPage(),
-        '/home': (context) => const CompanionModePage(),
-        '/homepage': (context) => const Homepage(),
-      },
+      home: HomePage(),
     );
   }
 }

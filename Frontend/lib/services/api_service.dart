5668e5349e942ed390f7e70ac5df70edf643e5f0
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-08-31 16:38:48 +0300

    threads fix

--- Frontend/lib/services/api_service.dart
+++ Frontend/lib/services/api_service.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  // static const String _baseUrl = 'http://192.168.43.219:5000'; // Update with your Flask server URL
-  static const String _baseUrl =  'https://backend-750j.onrender.com';
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-  final String _threadId = 'thread_id';
+  static const String _baseUrl = 'https://backend-750j.onrender.com';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<String> getChatbotResponseTherapist(String message) async {
     User? user = _auth.currentUser;
@@ -14,14 +13,19 @@ class ApiService {
       throw Exception('User not logged in');
     }
     String email = user.email!;
-    return _getChatbotResponse(message, _threadId, '/chat/therapist', email: email);
+    String threadId = await _getOrCreateThreadId('therapist');
+    return _getChatbotResponse(message, threadId, '/chat/therapist',
+        email: email);
   }
 
   Future<String> getChatbotResponseCompanion(String message) async {
-    return _getChatbotResponse(message, _threadId, '/chat/companion');
+    String threadId = await _getOrCreateThreadId('companion');
+    return _getChatbotResponse(message, threadId, '/chat/companion');
   }
 
-  Future<String> _getChatbotResponse(String message, String threadId, String endpoint, {String? email}) async {
+  Future<String> _getChatbotResponse(
+      String message, String threadId, String endpoint,
+      {String? email}) async {
     final url = Uri.parse('$_baseUrl$endpoint');
     final body = {
       'message': message,
@@ -52,6 +56,30 @@ class ApiService {
 
     if (response.statusCode != 200) {
       throw Exception('Failed to renew rate limit');
+    }
+  }
+
+  Future<String> _getOrCreateThreadId(String mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? threadId = prefs.getString('thread_id_$mode');
+
+    if (threadId == null) {
+      threadId = await _createThread();
+      await prefs.setString('thread_id_$mode', threadId);
+    }
+
+    return threadId;
+  }
+
+  Future<String> _createThread() async {
+    final url = Uri.parse('$_baseUrl/thread');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return data['thread_id'];
+    } else {
+      throw Exception('Failed to create thread');
     }
   }
 }

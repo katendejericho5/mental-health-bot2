ee2a949cc980072d7aac29fe4e232d170959ab35
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-09-02 15:27:44 +0300

    url

--- Frontend/lib/services/api_service.dart
+++ Frontend/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class ApiService {
   // static const String _baseUrl = 'http://192.168.43.219:5000'; // Update with your Flask server URL
-  static const String _baseUrl = 'https://54eb4eab-858c-4c1f-8835-a2ede0bc0de1-00-91p2lrp6g0ce.kirk.replit.dev';
+  static const String _baseUrl = 'https://backend-750j.onrender.com';
     final FirebaseAuth _auth = FirebaseAuth.instance;
   final String _threadId = 'thread_id';
 

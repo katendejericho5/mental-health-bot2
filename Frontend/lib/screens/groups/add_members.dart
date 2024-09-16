c90157168567ea852c9b1c4ef12915c9bb21a045
magalaReuben <magalareuben60@gmail.com>
2024-09-16 16:06:54 +0300

    new  screen changes

--- Frontend/lib/screens/groups/add_members.dart
+++ Frontend/lib/screens/groups/add_members.dart
@@ -1,3 +1,4 @@
+import 'package:WellCareBot/constant/size_config.dart';
 import 'package:WellCareBot/models/group_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -20,15 +21,49 @@ class _AddMembersScreenState extends State<AddMembersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(17, 6, 60, 1),
       appBar: AppBar(
-        title: Text('Add Members', style: GoogleFonts.poppins(),),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.check),
-            onPressed: _addSelectedMembers,
+          backgroundColor: Color.fromRGBO(17, 6, 60, 1),
+          elevation: 0,
+          title: Text(
+            'Add Members',
+            style: GoogleFonts.nunito(
+                color: Colors.white,
+                fontSize: getProportionateScreenWidth(20),
+                fontWeight: FontWeight.w700),
           ),
-        ],
-      ),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.check,
+                  color: Colors.white, size: getProportionateScreenHeight(35)),
+              onPressed: _addSelectedMembers,
+            ),
+          ],
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(getProportionateScreenHeight(15)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ))),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
@@ -36,7 +71,8 @@ class _AddMembersScreenState extends State<AddMembersScreen> {
             return Center(child: CircularProgressIndicator());
           }
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(child: Text('No users found',style:GoogleFonts.poppins()));
+            return Center(
+                child: Text('No users found', style: GoogleFonts.poppins()));
           }
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
@@ -44,6 +80,7 @@ class _AddMembersScreenState extends State<AddMembersScreen> {
               final user = snapshot.data!.docs[index];
               final userId = user.id;
               final userName = user['fullName'] ?? 'Unknown';
+              final imageUrl = user['profilePictureURL'] ?? '';
               final isSelected = selectedUsers.contains(userId);
               final isAlreadyMember = widget.group.memberIds.contains(userId);
 
@@ -51,18 +88,51 @@ class _AddMembersScreenState extends State<AddMembersScreen> {
                 return SizedBox.shrink(); // Don't show already added members
               }
 
-              return CheckboxListTile(
-                title: Text(userName, style: GoogleFonts.poppins()),
-                value: isSelected,
-                onChanged: (bool? value) {
-                  setState(() {
-                    if (value == true) {
-                      selectedUsers.add(userId);
-                    } else {
-                      selectedUsers.remove(userId);
-                    }
-                  });
-                },
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: Colors.white.withOpacity(0.7),
+                  child: CheckboxListTile(
+                    title: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              backgroundImage: NetworkImage(imageUrl)),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(userName,
+                                textAlign: TextAlign.start,
+                                style: GoogleFonts.nunito(
+                                    color: Colors.black,
+                                    fontSize: getProportionateScreenWidth(18),
+                                    fontWeight: FontWeight.w600)),
+                            SizedBox(
+                              width: getProportionateScreenWidth(190),
+                              child: Text(user['email'] ?? '',
+                                  style: GoogleFonts.nunito(
+                                      color: Colors.black,
+                                      fontSize: getProportionateScreenWidth(12),
+                                      fontWeight: FontWeight.w600)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    value: isSelected,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        if (value == true) {
+                          selectedUsers.add(userId);
+                        } else {
+                          selectedUsers.remove(userId);
+                        }
+                      });
+                    },
+                  ),
+                ),
               );
             },
           );
